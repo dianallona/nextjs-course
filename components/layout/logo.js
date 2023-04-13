266afdbe461f7e035334e9bde9715c4ea369b570
Diana Llona <dev.dianallona@gmail.com>
2023-04-13 08:53:27 +0800

    change navbar title

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./logo.module.css";
 
 const Logo = () => {
-  return <div className={classes.logo}>Logo</div>;
+  return <div className={classes.logo}>Diana's Blog</div>;
 };
 
 export default Logo;
